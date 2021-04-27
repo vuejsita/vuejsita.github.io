8bcff7782459871f291e80286fe33f97c2bfc5e7
thedarkknight197 <michele.vitiellobonaventura@gmail.com>
2021-04-27 21:41:10 +0200

    Created Card Chi-siamo

---
+++ components/chi-siamo-card/ChiSiamoCard.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="border border-gray-100 rounded-md p-8 m-8 bg-opacity-50 flex justify-center">
+        <!-- transform transition-all duration-300 ease-in scale-100 -->
+        <div class="text-center">
+            <img class="avatar" 
+                :src="this.user.imgUrl" 
+                alt="Michele Vitiello Bonaventura" 
+            />
+            <h1 class="text-xl mt-2 mb-2">{{user.name}}</h1>
+            <p class="italic mt-2 mb-2">"{{user.shortDescription}}.</p>
+            <div class="socialLink">
+                <a v-for="(social, k) in user.social" :key="k+social.key" :href="social.link" class="p-2 text-grey rounded-md">{{social.name}}</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import  Vue from 'vue'
+
+export default Vue.extend({
+    data(){
+      return {
+          user: Object
+      }
+    },
+    props: {
+        imgUrl: {type: String, required: true},
+        name: {type: String, required: true},
+        shortDescription: {type: String, required: true},
+        social: {type: Array, required: true},
+    },
+    mounted(): void {
+        this.user = this.$props;
+    },
+    methods : {
+      
+        
+    }
+})
+</script>
+
+<style lang="postcss" scoped>
+.avatar{
+    @apply rounded-full h-56 w-56 border-4 border-white hover:shadow-lg duration-500 ease-in scale-0;
+}
+
+</style>
---
+++ pages/chi-siamo.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <h1 class="text-center">Chi Siamo</h1>
+    <div class="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 justify-center">
+        <!-- TODO v-for for About card-->
+        <ChiSiamoCard v-bind="user[0]" />
+    </div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+// import ChiSiamoCard from '@/components/chi-siamo-card/ChiSiamoCard.vue';
+export default Vue.extend({
+    data() {
+        return {
+            user : [{
+                name: "Michele Vitiello Bonaventura",
+                shortDescription: "Short Description",
+                imgUrl: "http://mvbdev.cloud/images/mio.jpg",
+                social: [
+                    {name: 'instagram', link: 'instagram.com/thedarkknight197'},
+                    {name:'linkedin' , link: 'linkedin.com'},
+                    {name:'github', link: 'github.com/thedarkknight197'},
+                ]
+            }]
+        }
+    }
+})
+</script>
