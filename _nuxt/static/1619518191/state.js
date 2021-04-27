54abcbcccae82b042c9c2ab5d306a2e7d9307949
jfet97 <jfet97@users.noreply.github.com>
2021-04-27 10:09:52 +0000

    deploy: 977b694b7bc95fce341c817e991769b0cb974868

---
+++ _nuxt/static/1619518191/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619518191",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1b2cc18b"}}}}(null,"\u002F"));
