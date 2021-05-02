81db83af067493346076811b7905cb25525293f9
leo_gas96 <leonardogasparini96@gmail.com>
2021-05-02 21:52:23 +0200

    removed useless style

--- pages/index.vue
+++ pages/index.vue
@@ -32,12 +32,7 @@ export default Vue.extend({
 </script>
 
 <style>
-/*
-Button Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
+
 .container {
   margin: 0 auto;
   min-height: 100vh;
