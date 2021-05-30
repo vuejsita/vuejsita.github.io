4989341ded4189423478b7251262f83661b929b4
leogasparini <leogasparini@users.noreply.github.com>
2021-05-30 19:30:03 +0000

    deploy: 96b0015383c2bab9d0161a16c82804e6db7a3405

--- _nuxt/static/1620047535/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620047535",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ad3e6bba"}}}}(null,"\u002F"));
