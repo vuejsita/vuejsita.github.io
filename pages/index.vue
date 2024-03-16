dd45a6b32d1ebf5de408ed9803423ac685d01300
Rosario <sariodesign.it@gmail.com>
2024-03-16 11:22:38 +0100

    #39 - Aggiunto colore in riferimento a Vue

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
       >
         <div
           id="hero__halo--top"
-          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#42b883] to-[#42b883] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
         />
       </div>
       <div>
@@ -72,7 +72,7 @@
       >
         <div
           id="hero__halo--bottom"
-          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#42b883] to-[#42b883] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
         />
       </div>
     </main>
