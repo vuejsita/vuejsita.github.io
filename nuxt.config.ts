7a6625343965eb77a113a9c85c12c0ad8dae8555
Matteo Bilotta <me@byloth.dev>
2024-03-16 11:38:22 +0100

    add: Introdotto "Umami" come servizio di Analytics.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  extends: ["nuxt-umami"],
   modules: [
     '@nuxt/content',
     '@nuxt/ui'
