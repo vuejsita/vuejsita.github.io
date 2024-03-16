735f04525d97dc2415f097cfa80a0cbc946ea982
Mattia Dapino <mattia@dapino.dev>
2024-03-16 11:44:21 +0100

    Aggiunto titolo e favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Vue.js Italia",
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
+    },
+  },
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/content',
-    '@nuxt/ui'
-  ],
-  typescript: { typeCheck: true }
-})
+  modules: ["@nuxt/content", "@nuxt/ui"],
+  typescript: { typeCheck: true },
+});
